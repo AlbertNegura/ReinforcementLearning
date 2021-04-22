729d54c3416003f2f17605eea179c8495c0c2178
I6145864 <a.negura@student.maastrichtuniversity.nl>
2021-04-22 10:04:31 +0200

    Basic Breakout

---
+++ breakout.py
@@ -0,0 +1,33 @@
+import numpy as np
+import gym
+from models import build_atari_model
+from agents import build_rl_agent
+from tensorflow.keras.optimizers import Adam
+
+env = gym.make('Breakout-v0')
+episodes = 10
+verbosity = 1
+visualize = True
+
+height, width, channels = env.observation_space.shape
+actions = env.action_space.n
+
+model = build_atari_model(height, width, channels, actions)
+dqn = build_rl_agent(model, actions)
+
+dqn.compile(Adam(lr=0.001))
+dqn.fit(env, nb_steps=40000, visualize=True, verbose=1)
+
+for episode in range(1, episodes):
+    state = env.reset()
+    finished = False
+    score = 0
+
+    while not finished:
+        env.render()
+        state, reward, finished, info = env.step(env.action_space.sample())
+        score+=reward
+
+    print('Episode: {}\nScore: {}'.format(episode, score))
+
+env.close()
---
+++ models.py
@@ -0,0 +1,15 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+
+
+
+def build_atari_model(height, width, channels, actions):
+    model = Sequential()
+    model.add(Conv2D(32, (8,8), strides=(4,4), activation='relu', input_shape=(3, height, width, channels)))
+    model.add(Conv2D(64, (4,4), strides=(2,2), activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(actions, activation='linear'))
+    return model
